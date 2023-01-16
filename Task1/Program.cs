d67c6b2f632fece225952830f03674db201e3e29
LIUBOV <lmshumlyakovskaya@gmail.com>
2023-01-16 17:30:29 +0600

    продолжили done task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,92 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*
+Console.Clear();  
 Console.WriteLine("Hello, World!");
+*/
+// ctrl + / 
+// Console.Clear();
+// int a = 1, b = 2;
+// Console.WriteLine($"{a}, {b}");
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int n = int.Parse(Console.ReadLine());
+// Console.WriteLine($"Результат: {n * 3}");
+// Результат: 15 
+
+// Console.Clear();
+// Console.Write("Введите 1-ое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 2-ое число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// if (n > m)
+//     Console.WriteLine(n);
+// else if (n < m)    
+// if (n < m)
+//     Console.WriteLine(m);
+// else
+//     Console.WriteLine("=");
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Результат: {n * n}");
+
+// Задача 1
+
+// Console.Clear();
+// Console.Write("Введите 1-ое число");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 2-ое число");
+// int m = Convert.ToInt32(Console.ReadLine());
+// if (m * m == n)
+//     Console.WriteLine("Yes");
+// else
+//     Console.WriteLine("No");
+
+// Console.Clear();
+// Console.Write("Введите день недели: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// && -и(логическое умножение
+// || -или(логическое сложение)
+// True - 1
+// False - 0
+//        1  +  0  = 1(True)
+// while (n < 1 || n > 7)
+//   {
+//     Console.Write("Вы ошиблись!\nВведите день недели: ");
+//     n = Convert.ToInt32(Console.ReadLine());
+//   }
+// if (n == 1)
+//       Console.WriteLine("Понедельник");
+// else if (n == 2)
+//       Console.WriteLine("Вторник");
+// else if (n == 3)
+//       Console.WriteLine("Среда");   
+// else if (n == 4)
+//       Console.WriteLine("Четверг");
+// else if (n == 5)
+//       Console.WriteLine("Пятница");
+// else if (n == 6)
+//       Console.WriteLine("Суббота");
+// else
+//       Console.WriteLine("Воскресенье");
+
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int m = n * (-1);
+// while (m <= n)
+// {
+//     Console.Write($"{m} ");
+//     m = m + 1;      m++;     
+// }
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(n % 10);
+
+    
+
