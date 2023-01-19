a6889da98295c950a1bb39b20985d61036cdf91d
LIUBOV <lmshumlyakovskaya@gmail.com>
2023-01-19 15:00:21 +0600

    задача 4

---
+++ HomeTask4/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int d = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int e = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-е число: ");
+int f = Convert.ToInt32(Console.ReadLine()), max = d;
+if (e >= max && e >= f)
+   Console.Write(e);
+else 
+   Console.Write(f);
