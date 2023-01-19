128891653ad20db193635058462ddc545724fb52
LIUBOV <lmshumlyakovskaya@gmail.com>
2023-01-19 16:11:22 +0600

    задача 6

---
+++ HomeTask6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Hello, World!");
+// ЗАДАЧА 6: Напишите программу, которая на вход принимает число и выдаёт,
+//           является ли оно чётным (делится ли оно на два без остатка).
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());  
+if(a % 2 == 0)
+   Console.Write("Чётное");
+else 
+   Console.Write("Нечётное");
+
