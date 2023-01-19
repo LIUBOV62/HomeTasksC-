d4623e4667a8f21513a7c88828e90d629532f243
LIUBOV <lmshumlyakovskaya@gmail.com>
2023-01-19 16:40:49 +0600

    задача 8

---
+++ HomeTask8/Program.cs
@@ -0,0 +1,11 @@
+﻿// ЗАДАЧА 8: Напишите программу, которая на вход принимает
+// //           число (N), а на выходе все чётные от 1 до N.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()),count = 2;
+while (count <= n)
+{
+      Console.Write($"{count} ");
+      count = count + 2;
+}
