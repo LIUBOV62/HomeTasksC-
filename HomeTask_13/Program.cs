c50eaec7ab97d8f7b162337a73ec800d00915fc1
LIUBOV <lmshumlyakovskaya@gmail.com>
2023-01-21 15:46:01 +0600

    задача13

---
+++ HomeTask_13/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+Console.Clear();
+Console.Write("Введите 1-ое число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 100)
+Console.WriteLine("Третьей цифры нет ");
+else
+{
+        while (n >= 1000)
+        {
+                n = n / 10;
+        }
+        Console.WriteLine(n);
+        Console.WriteLine(n % 10);
+}       
