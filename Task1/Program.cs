917c3fded5e4f318deefc524ff390c1eaa221bcc
LIUBOV <lmshumlyakovskaya@gmail.com>
2023-01-24 13:49:34 +0600

    объединение элементов массива

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -364,17 +364,17 @@ Console.WriteLine("Hello, World!");
 // Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет,
 //           является ли оно палиндромом. 14212 -> нет, 12821 -> да, 23432 -> да
 
-Console.Clear();
-Console.Write("Введите пятизначное число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int n1 = n / 10000;
-int n2 = (n / 1000) % 10;
-int n4 = (n % 100) / 10;
-int n5 = n % 10;
-if (n1 == n5 && n2 == n4)
-       Console.WriteLine("Палиндром");
-else      
-       Console.WriteLine("НЕ палиндром");
+// Console.Clear();
+// Console.Write("Введите пятизначное число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int n1 = n / 10000;
+// int n2 = (n / 1000) % 10;
+// int n4 = (n % 100) / 10;
+// int n5 = n % 10;
+// if (n1 == n5 && n2 == n4)
+//        Console.WriteLine("Палиндром");
+// else      
+//        Console.WriteLine("НЕ палиндром");
 
 
 //  Задача 21: Напишите программу, которая принимает на вход координаты двух точек 
@@ -406,3 +406,33 @@ else
 // int n = Convert.ToInt32(Console.ReadLine());
 // for (int i = 1; i <= n; i++)
 // Console.Write($"{Math.Pow(i, 3)} ");
+//  Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[n];
+// for (int i = 0; i < n; i++)
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+ 
+// int maxSumma = 0;
+// for (int i = 1; i < array.Length - 1; i++)
+// {
+//     int sum = array[i - 1] + array[i] + array[i + 1];
+//     if (sum > maxSumma)
+//         maxSumma = sum;
+// }
+// if (array[0] + array[1] + array[array.Length - 1] > maxSumma)
+//     maxSumma = array[0] + array[1] + array[array.Length - 1];
+ 
+// if (array[array.Length - 1] + array[array.Length - 2] + array[0] > maxSumma)
+//     maxSumma = array[array.Length - 1] + array[array.Length - 2] + array[0];
+ 
+// Console.WriteLine(maxSumma);
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-10, 11); // [-10, 10]
+
+Console.WriteLine($"Результат: [{string.Join("    ", array)}]");
