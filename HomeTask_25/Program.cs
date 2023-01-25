b06cd2c75d92ecfc248de37a2d0b793ef6974722
LIUBOV <lmshumlyakovskaya@gmail.com>
2023-01-25 13:22:09 +0600

    задача 25

---
+++ HomeTask_25/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+ Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int temp =n;
+
+for (int i = 1; i < m; i++)
+    temp = n * temp;
+
+Console.WriteLine($"{temp}");
