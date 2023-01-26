14152c22e1a18ca6132957fbf86329cf84020c3d
LIUBOV <lmshumlyakovskaya@gmail.com>
2023-01-26 21:06:59 +0600

    задача 29(массив из 8 элементов ввод элементов массива с клавиатуры)

--- HomeTask_29/Program.cs
+++ HomeTask_29/Program.cs
@@ -3,8 +3,20 @@
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит
 //            их на экран(ввод пользователя с клавиатуры).
 //            1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+Console.Clear();
+// int[] array = new int[8];
+// for(int i = 0; i < array.Length; i++)
+//     array[i] = new Random().Next(1, 50);
+// Console.WriteLine($"Результат: [{string.Join(",", array)}]");
+
+
 Console.Clear();
 int[] array = new int[8];
-for(int i = 0; i < array.Length; i++)
-    array[i] = new Random().Next(1, 50);
-Console.WriteLine($"Результат: [{string.Join(",", array)}]");
+
+for(int i = 0; i < array.Length; i++) 
+{
+       Console.Write($"Введите элемент массива под индексом {i}: ");
+       array[i] = Convert.ToInt32(Console.ReadLine());
+}
+       Console.WriteLine($"Результат: [{string.Join(",", array)}]");
