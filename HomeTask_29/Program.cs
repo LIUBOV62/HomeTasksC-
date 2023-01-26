ebf6fdd1074a54b8da940f4689bdaeab8026d416
LIUBOV <lmshumlyakovskaya@gmail.com>
2023-01-26 11:37:38 +0600

    задача 29

---
+++ HomeTask_29/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит
+//            их на экран(ввод пользователя с клавиатуры).
+//            1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+Console.Clear();
+int[] array = new int[8];
+for(int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 50);
+Console.WriteLine($"Результат: [{string.Join(",", array)}]");
