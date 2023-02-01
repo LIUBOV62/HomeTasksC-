6a93e722aaed365303ace5f2ef1c732653ad437d
LIUBOV <lmshumlyakovskaya@gmail.com>
2023-02-01 16:12:50 +0600

    namberA + namberB

--- Lecture/Program.cs
+++ Lecture/Program.cs
@@ -1,7 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
-Console.Clear();
-Console.WriteLine("Введите ваше имя: ");
-string username = Console.ReadLine();
-Console.WriteLine("Привет! ");
-Console.WriteLine(username);
+// Console.Clear();
+// Console.WriteLine("Введите ваше имя: ");
+// string username = Console.ReadLine();
+// Console.WriteLine("Привет! ");
+// Console.WriteLine(username);
+
+ Console.Clear();
+ int namberA = 31;
+ int namberB = 5;
+ int result = namberA + namberB;
+ Console.WriteLine(result);
