dcfabf62efe9744e4b55aa2f6977724f43f6e937
LIUBOV <lmshumlyakovskaya@gmail.com>
2023-02-02 23:15:57 +0600

    найти элемент массива, равный find

--- Lecture_2/Program.cs
+++ Lecture_2/Program.cs
@@ -26,17 +26,35 @@
 // int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 // Console.WriteLine(max);
 
+// Console.Clear();
+// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
+//  int[]array = {13, 2, 23, 41, 15, 6, 27, 8, 19};
+//  int max = Max(
+//      Max(array[0], array[1], array[2]),
+//      Max(array[3], array[4], array[5]),
+//      Max(array[6], array[7], array[8]));
+//      Console.WriteLine(max);
+
 Console.Clear();
-int Max(int arg1, int arg2, int arg3)
+int [] array = {1, 12, 31, 4, 18, 15, 16, 17, 18 };
+int n = array.Length;
+int find = 17;
+int index = 0;
+
+while (index < n)
 {
-    int result = arg1;
-    if (arg2 > result) result = arg2;
-    if (arg3 > result) result = arg3;
-    return result;
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //  index = index + 1;
+    index++;
+        
 }
- int[]array = {13, 2, 23, 41, 15, 6, 27, 8, 19};
- int max = Max(
-     Max(array[0], array[1], array[2]),
-     Max(array[3], array[4], array[5]),
-     Max(array[6], array[7], array[8]));
-     Console.WriteLine(max);
